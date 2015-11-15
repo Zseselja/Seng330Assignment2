60c7f5df7fa10d0b82d66e8594cb9888d80e0c1d
Zseselja <zseselja@uvic.ca>
2015-11-14 17:55:21 -0800

    First Push to Repo.

---
+++ Point.h
@@ -0,0 +1,32 @@
+/*
+ * Point.h
+ *
+ *  Created on: Nov 14, 2015
+ *      Author: Zachary Seselja
+ */
+
+#ifndef POINT_H_
+#define POINT_H_
+
+namespace model
+{
+class Point
+	{
+	private:
+		int x;
+		int y;
+
+	public:
+		Point( int x ,  int y){
+			this->x = x;
+			this->y = y;
+
+
+		}
+		/* data */
+	};
+}
+
+
+
+#endif /* POINT_H_ */
---
+++ ShipType.h
@@ -0,0 +1,15 @@
+/*
+ * ShipType.h
+ *
+ *  Created on: Nov 14, 2015
+ *      Author: Zachary Seselja
+ */
+
+#ifndef SHIPTYPE_H_
+#define SHIPTYPE_H_
+
+
+
+
+
+#endif /* SHIPTYPE_H_ */
